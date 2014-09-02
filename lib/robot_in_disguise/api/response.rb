deb0b3716afe8ef6e22051979bd33996e568fbdf
Mario Zigliotto <mariozig@gmail.com>
2014-09-02 08:57:06 -0700

    Begining work on generic response class.

--- lib/robot_in_disguise/api/response.rb
+++ lib/robot_in_disguise/api/response.rb
@@ -1,5 +1,18 @@
 module RobotInDisguise::API
   # Generic response from API
   class Response
+    # @return [RobotInDisguise::Client] The client used to generate the response.
+    attr_reader :client
+
+    # @return [String, Hash] The response body from TFX.
+    attr_reader :body
+    
+    def initialize(faraday_response, client)
+      @client = client
+      @response = faraday_response
+
+      @response.on_complete do 
+      end
+    end
   end
 end
