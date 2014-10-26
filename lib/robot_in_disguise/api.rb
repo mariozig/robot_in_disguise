2185443b8820a67471e6966b4c2c84f5dee3137b
Mario Zigliotto <mario_zigliotto@intuit.com>
2014-10-26 11:40:18 -0700

    Get rid of Posts

--- lib/robot_in_disguise/api.rb
+++ lib/robot_in_disguise/api.rb
@@ -2,7 +2,6 @@ require 'robot_in_disguise/search'
 
 module RobotInDisguise
   module API
-    include RobotInDisguise::Posts
     include RobotInDisguise::Search
   end
 end
--- lib/robot_in_disguise/posts.rb
+++
@@ -1,6 +0,0 @@
-require 'robot_in_disguise/request'
-
-module RobotInDisguise
-  module Posts
-  end
-end
