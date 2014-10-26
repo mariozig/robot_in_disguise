020a708076571a01bd205b588e20de7c16b2c504
Mario Zigliotto <mario_zigliotto@intuit.com>
2014-10-26 12:33:28 -0700

    Use some metaprogramming...
    Upon initialization, the Post class uses it's attr keys to add and return appropriate attributes

--- lib/robot_in_disguise/post.rb
+++ lib/robot_in_disguise/post.rb
@@ -11,10 +11,20 @@ module RobotInDisguise
     # @return [RobotInDisguise::Post]
     def initialize(attrs = {})
       @attrs = attrs || {}
+      define_attribute_methods(@attrs.keys)
     end
 
-    # @return [String] The post's full title
-    def title
+    private
+
+    # Dynamically adds methods to the Post class keyed
+    # off of TFX's response
+    #
+    # @param post_attributes [Array] A list of all attributes
+    def define_attribute_methods(post_attributes)
+      post_attributes.each do |post_attr|
+        post_attr = post_attr.to_sym
+        self.class.send(:define_method, post_attr) { @attrs[post_attr] }
+      end
     end
   end
 end
