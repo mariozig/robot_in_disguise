0766f418c1cd247f3292a62c657a7456c53727aa
Mario Zigliotto <mario_zigliotto@intuit.com>
2014-11-04 07:27:21 -0800

    Whitespace for rubocop

--- lib/robot_in_disguise/utils.rb
+++ lib/robot_in_disguise/utils.rb
@@ -3,28 +3,29 @@ require 'robot_in_disguise/request'
 module RobotInDisguise
   module Utils
     private
-      # Take a URI or string and return it's content_id
-      #
-      # @param object [String, URI, RobotInDisguise::Post] A String, URI, or Post object.
-      # @return [String]
-      def extract_content_id(object)
-        case object
-        when ::String
-          object
-        when URI
-          object.path.split('/').last.to_s
-        when RobotInDisguise::Post
-          object.content_id
-        end
-      end
 
-      # @param request_method [Symbol]
-      # @param path [String]
-      # @param options [Hash]
-      # @param klass [Class]
-      def perform_with_object(request_method, path, options, klass)
-        request = RobotInDisguise::Request.new(self, request_method, path, options)
-        request.perform_with_object(klass)
+    # Take a URI or string and return it's content_id
+    #
+    # @param object [String, URI, RobotInDisguise::Post] A String, URI, or Post object.
+    # @return [String]
+    def extract_content_id(object)
+      case object
+      when ::String
+        object
+      when URI
+        object.path.split('/').last.to_s
+      when RobotInDisguise::Post
+        object.content_id
       end
+    end
+
+    # @param request_method [Symbol]
+    # @param path [String]
+    # @param options [Hash]
+    # @param klass [Class]
+    def perform_with_object(request_method, path, options, klass)
+      request = RobotInDisguise::Request.new(self, request_method, path, options)
+      request.perform_with_object(klass)
+    end
   end
 end
